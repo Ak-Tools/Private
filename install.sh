0c116c84874231fa56bb0c2ad12663415fe4f2c5
Ak-Tools <84968965+Ak-Tools@users.noreply.github.com>
2022-01-29 19:33:57 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+cd $HOME/metasploit-framework
+sed '/rbnacl/d' -i Gemfile.lock
+sed '/rbnacl/d' -i metasploit-framework.gemspec
+gem install bundler
+sed 's|nokogiri (1.*)|nokogiri (1.8.0)|g' -i Gemfile.lock
+
+gem install nokogiri -v 1.8.0 -- --use-system-libraries
+
+gem install actionpack
+bundle update activesupport
+bundle update --bundler
